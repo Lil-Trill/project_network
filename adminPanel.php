37fa7c120de29881d771d1b9784a48609f323163
Azart <danil.yurin.1990@mail.ru>
2023-05-19 23:12:05 +0300

    add button deleted

--- adminPanel.php
+++ adminPanel.php
@@ -13,7 +13,27 @@
                 require './config.php';
                 $connect = inc();
 
-                $sql = 
+                $sql = "SELECT * 
+                FROM `books`
+                JOIN book_author ON books.id_book = book_author.id_book
+                JOIN authors ON authors.id_authors = book_author.id_author";
+
+                $data = mysqli_query($connect,$sql);
+
+                while($row = $data->fetch_assoc()){
+                    if($row["image_path"] == null)$row["image_path"] = "./images/nf.png";
+                    echo "<div class='item-book' data-id='$row[id_book]'>
+                    <img class='book-image' src='$row[image_path]' alt=''>
+                    <h2 class='book-title'>$row[name]</h2>
+                    <div class='author-name'>
+                        <p/>Автор:</p>
+                        <p/ class='lname'>$row[lname]</p>
+                        <p/ class='fname'>$row[fname]</p>
+                        <p/ class='surname'>$row[surname]</p>
+                    </div>
+                    <button class='deleted'>удалить книгу</button>
+                    </div>";
+                }
             ?>
         </div>
     </main>
